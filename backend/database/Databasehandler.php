0aa60dbb708c0a17bf6fb501d9ced85a9735d620
T.Behrens <behrens@traum-ferienwohnungen.de>
2014-03-24 18:51:28 +0100

    Adds the DatabaseHandler which collect the request from javascript and ask the db for a return or insert some stuff get from js parameters.

---
+++ backend/database/Databasehandler.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: T.Behrens
+ * Date: 21.03.14
+ * Time: 10:57
+ */
+
+namespace backend\database;
+
+
+class Databasehandler
+{
+
+	private $sql = array();
+	private $arr;
+
+	public function __construct(array $arr = null)
+	{
+		if ($arr !== null) {
+			$this->arr = $arr;
+			foreach ($arr['column'] as $column => $value) {
+				$this->sql[] = "Insert INTO $arr[table]
+						 	  ($column)
+							  VALUES
+							  ($value)";
+			}
+		}
+		$this->sql = 'SELECT *
+					  FROM *
+					  WHERE true';
+	}
+
+	public function execute()
+	{
+		$db = $this->connect();
+		$result = $this->doQuery($db);
+
+		return $result;
+	}
+
+	private function connect()
+	{ // host user passwort database
+		$db = mysqli_connect("localhost", "root", "root", "simulator");
+		if (!$db) {
+			exit("Verbindungsfehler: " . mysqli_connect_error());
+		}
+
+		return $db;
+	}
+
+	private function doQuery($db)
+	{
+		foreach ($this->sql as $sql) {
+			$result = mysqli_query($db, $sql);
+		}
+
+		return $result;
+	}
+}
+
+$go = new Databasehandler();
+$go->execute();
