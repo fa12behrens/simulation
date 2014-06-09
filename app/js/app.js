d8a1fb05e9ec2b304754f1454dd56b3564e6790a
T.Behrens <behrens@traum-ferienwohnungen.de>
2014-06-09 22:34:30 +0200

    Add change sql schema and remove view 1.

--- app/js/app.js
+++ app/js/app.js
@@ -11,8 +11,6 @@ var simulationApp = angular.module('simulationApp', [
 
 // Route partials which are shown with ng-view, also adds a controller to this partial.
 simulationApp.config(['$routeProvider', function ($routeProvider) {
-	$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html'});
-	$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html'});
 	$routeProvider.when('/view10', {templateUrl: 'partials/partial10.html', controller: 'RoundController'});
-	$routeProvider.otherwise({redirectTo: '/view1'});
+	$routeProvider.otherwise({redirectTo: '/view10'});
 }]);
